a8e759b318aba09bedd0d7d60388d11b692834c4
Lawi Kiprotich <fleeklawi@gmail.com>
2024-05-05 04:52:10 +0300

    Created model - Properties

--- realtor/models.py
+++ realtor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Properties(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    address = models.CharField(max_length=250)
+    price = models.IntegerField()
+    size = models.CharField(max_length=50, blank=True)
+    bedrooms = models.IntegerField(blank=True)
+    bathrooms = models.IntegerField(blank=True)
+    amentities = models.CharField(max_length=100, blank=True)
+    status = models.CharField(max_length=50)
+    year_built = models.IntegerField()
+    images = models.ImageField(upload_to='/images/props')
+    location = models.CharField(max_length=100)
+    features = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title.split()[0]
+
