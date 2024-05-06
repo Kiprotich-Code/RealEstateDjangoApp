fcbd4f47a9d15fd44a0ae07f27159b15585acb80
Lawi Kiprotich <fleeklawi@gmail.com>
2024-05-06 10:01:31 +0300

    disclaimer

--- realtor/urls.py
+++ realtor/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", views.realtor, name="realtor"),
@@ -9,4 +10,10 @@ urlpatterns = [
     path("property_list/", views.PropertyListView.as_view(), name="property_list"),
     path("property_details/<pk>", views.PropertyDetailView.as_view(), name="property_details"),
     path("update_property/<pk>", views.PropertyUpdateView.as_view(), name="update_property"),
+
+    #sphaghetti
+    path('orders/', TemplateView.as_view(template_name="realtors/orders.html"), name="orders"),
+    path('callbacks/', TemplateView.as_view(template_name="realtors/callbacks.html"), name="callbacks"),
+    path('messages/', TemplateView.as_view(template_name="realtors/messages.html"), name="messages"),
+    path('analytics/', TemplateView.as_view(template_name="realtors/analytics.html"), name="analytics"),
 ]
